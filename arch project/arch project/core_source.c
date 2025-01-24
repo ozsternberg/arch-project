0f7849d2c676373680fc63a967540ed23aa0b4da
ozsternberg <ozsh1113@gmail.com>
2025-01-24 16:14:57 +0200

    Updated mem access

--- arch project/arch project/core_source.c
+++ arch project/arch project/core_source.c
@@ -35,15 +35,15 @@ cache_query_rsp_s cache_query(int dsram[][], tsram_entry tsram[], int addr,opcod
 
     else
     {
-        if(hit) // Wr hit
+        if(hit && data_state != Invalid) // Wr hit
         {
-            if (progress_clk)
+            if (progress_clk && data_state != Shared)
             {
                 dsram[cache_addr.set][cache_addr.offset] = data;
                 tsram[cache_addr.set].state = Modified;
             }
 
-            hit_type = kHit;
+            hit_type = data_state == Shared ? kWrHitShared : kHit;
         }
 
         else
@@ -55,4 +55,71 @@ cache_query_rsp_s cache_query(int dsram[][], tsram_entry tsram[], int addr,opcod
     return  cache_query_rsp;
 }
 
-void handle_mem
+mem_rsp_s handle_mem(int dsram[][], tsram_entry tsram[], int addr,opcode op, int data, int progress_clk,  cache_state_t * cache_state, bus_cmd_s bus, int gnt) 
+{
+    mem_rsp_s mem_rsp = {0};
+    cache_query_rsp_s cache_query_rsp = cache_query(dsram,tsram,addr,op,data,progress_clk);
+    cache_hit_t hit_type = cache_query_rsp.hit_type;
+    mem_rsp.data         = cache_query_rsp.data;
+    int req = op == sw || op == lw || cache_state != kIdle ? 1 : 0;
+
+    bus_routine_rsp_s bus_routine_rsp = bus_routine(cache_state, hit_type, bus, gnt, mem_rsp, req);
+    
+    switch (cache_state)
+    {
+        case kIdle:
+            if (hit_type == kHit) 
+            break; // If hit do nothing
+            
+            // If gnt is 0, stall
+            if (gnt == 0) 
+            {
+                mem_rsp.stall = 1;
+                *cache_state = kWaitForGnt;
+                break;
+            }
+            
+            if (hit_type == kWrHitShared) break; //If gnt == 1 and hit is shared do stay Idle
+
+            else // If gnt == 1 and miss wait for flush or send
+            {
+                mem_rsp.stall = 1;
+                *cache_state = kWaitForFlush;
+                break;
+            }
+
+        case kWaitForGnt:
+            mem_rsp.stall = 1;
+            if (gnt == 0) break; // If gnt is 0, stall
+
+            if (hit_type == kWrHitShared) //If gnt == 1 and hit is shared do stay Idle
+            {
+                *cache_state = kIdle;
+                break;
+            }
+
+            else // If gnt == 1 and miss, wait for flush receive or send
+            {
+                *cache_state = kWaitForFlush;
+                break;
+            }
+
+        case kWaitForFlush:
+            mem_rsp.stall = 1;
+            if (bus_routine_rsp.data_rtn == 0) break; // If no data is returned, stall
+
+            if (bus_routine_rsp.data_rtn == 1)
+            {
+                if (bus_routine_rsp.bus_cmd.bus_addr != addr) 
+                {
+                    printf("Data is received from wrong address\n");
+                }
+                *cache_state = kIdle;
+            }
+
+            break;
+    }
+
+    
+
+}
